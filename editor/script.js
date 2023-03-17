e7ef484a4004d496b6af720e32e5fbdcb48a830a
Dilute Oxygen <vikrantsinghtomar011@gmail.com>
2023-03-18 01:48:12 +0530

    Add files via upload
    Index.html
    Style.css
    Script.js
    (this is just the test preview for now)
    
    Co-Authored-By: ifeegoo <root@ifeegoo.com>

---
+++ editor/script.js
@@ -0,0 +1,9 @@
+var editor = CodeMirror.fromTextArea(document.getElementById("editor"), {
+    lineNumbers: true,
+    mode: "javascript",
+    theme: "3024-night",
+    scrollbarStyle: "null",
+    lineWrapping: true
+});
+
+
